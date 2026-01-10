3c848f04972d4b50738b8294d745ba8fd4ce151a
zohir hamid <zohirhamid011@gmail.com>
2026-01-10 18:19:06 +0000

    password change and reset

--- config/settings.py
+++ config/settings.py
@@ -122,5 +122,7 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 LOGIN_URL = '/login/'
 
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
 # OTHER
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
