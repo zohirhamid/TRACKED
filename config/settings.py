d12a80f407fd254f20bb2f71cc96a61af274d4d4
zohir hamid <zohirhamid011@gmail.com>
2026-01-12 18:58:16 +0000

    update login redirect

--- config/settings.py
+++ config/settings.py
@@ -152,8 +152,8 @@ USE_TZ = True
 
 # Authentication URLs
 LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
-LOGIN_URL = '/login/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
+LOGIN_URL = '/accounts/login/'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
